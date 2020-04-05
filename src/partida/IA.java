d6f598a512310ed02a67a0dfbee48281ca5987c6
Asier <asier.oyanguren@gmail.com>
2020-04-05 16:28:31 +0200

    Creado el método abstracto jugar() en la clase IA

--- src/partida/IA.java
+++ src/partida/IA.java
@@ -1,7 +1,8 @@
 package partida;
 
-public class IA {
+public abstract class IA {
 
+	public abstract void jugar();
 	
 	public double obtenerColumnaAleatoria(double min, double max){
 	    return (Math.random()*((max-min)+1))+min;
--- src/partida/IADificil.java
+++ src/partida/IADificil.java
@@ -2,4 +2,7 @@ package partida;
 
 public class IADificil extends IA {
 
+	public void jugar() {
+		System.out.println("Hola");
+	}
 }
